305c457da4be4a5a9bf0db71e2556ae36a9b6fe4
Angel Lopez <angeldavidlopezcorona403@gmail.com>
2026-07-23 12:59:10 -0700

    Quita parametro sin utilizar en index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const $shareBtn = document.getElementById("share-btn");
 const $sharePanel = document.getElementById("share-panel");
 
-$shareBtn.addEventListener("click", (e) => {
+$shareBtn.addEventListener("click", () => {
   const isVisible = $sharePanel.classList.toggle("card__share-panel--visible");
   $shareBtn.setAttribute("aria-expanded", isVisible);
   $shareBtn.classList.toggle("card__share-btn--clicked");
